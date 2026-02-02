5baf89881f0c3f4e212c7dab8fbdad390522d56b
glourvell <kimat14in2020@gmail.com>
2026-02-02 19:42:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+pin = 3030
+Balance = 500
+
+#menu
+def menu():
+        print("ATM MENU")
+        print("1. Check balance")
+        print("2. Withdraw money")
+        print("3. Deposit money")
+        print("4. Change PIN")
+        print("Please choose a number to select an option below")
+
+        user_choice = int(input())
+        if user_choice == 1:
+             print('Your balance is Ksh:', Balance)
+        elif user_choice == 2:
+             
+            
+
+
+def pin_verifier(pin):
+    print("Enter your PIN:")
+    user_pin = int(input())
+    if user_pin == pin:
+        print("Your PIN is correct")
+        menu()
+    else:
+        def attempt():
+            i = 1
+            while i < 4:
+                print(f"You have entered a wrong PIN, you have {4 - i} attempts left")
+                if user_pin == pin:
+                    print("Your PIN is correct")
+                    return
+                i += 1
+                print("You are out of attempts")
+
+        attempt()
+
+
+
+pin_verifier(pin)
+ 
+
+
+
+
+
